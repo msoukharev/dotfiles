3e1eb84f9369313ea5857e5b68fc2b240fadc26c
Max Soukharev <maxs3v@gmail.com>
2020-11-14 01:06:15 -0500

    Fix git aliases

--- dotfiles/gitalias.sh
+++ dotfiles/gitalias.sh
@@ -1,20 +1,24 @@
 alias gita="git add ."
 
+gitc() {
+    git commit -m $@
+}
+
 # git add . ; git commit -m $1
 gitac() {
-    git add . ; git commit -m $1
+    git add . ; git commit -m $@
 }
 
 # git push origin $1
 gitps() {
-    git push origin $1
+    git push origin $@
 }
 
 # git pull origin $1
 gitpl() {
-    git pull origin $1
+    git pull origin $@
 }
 
 gitpushso() {
-    git push --set-upstream origin $1
+    git push --set-upstream origin $@
 }
