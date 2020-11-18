8db622de998cc8b74f66b4a3d533c46b195b1e3f
Max Soukharev <maxs3v@gmail.com>
2020-11-18 01:06:40 -0500

    Fix remote install

--- remote_install.sh
+++ remote_install.sh
@@ -1,5 +1,14 @@
-echo "Remote installing msoukharev/dotfiles"
+echo "Remote installing msoukharev/dotfiles!"
 git clone https://github.com/msoukharev/dotfiles ~/.dotfiles
+if [ -d "~/.dotfiles" ]; then
+    echo "Successfully installed dotfiles!"
+else
+    echo "Failed to install dotfiles."
+fi
 
-echo "Installing msoukharev/dotfiles"
-sh ~/.dotfiles/install.sh
+export ZSH_DOTFILES="~/.dotfiles/"
+
+chmod -R 755 $ZSH_DOTFILES
+
+echo "Setting up the environment."
+sh $ZSH_DOTFILES/install.sh
