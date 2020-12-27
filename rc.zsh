3782786d9c977c007b2daaec804e9a8c418f556f
Max Soukharev <dev.soukharev@gmail.com>
2020-12-26 22:25:38 -0500

    Refactor

--- rc.zsh
+++ rc.zsh
@@ -1,12 +1,16 @@
-# Source configs
-__dotfile_source_path Config
-
 # Dynamic prompt string reevaluation
 setopt PROMPT_SUBST
 
-# Source theme
+# Source the theme, source source default if no theme found
 if [ -n $theme ]; then
     __dotfile_source_path Themes User $theme >/dev/null 2>&1 || \
     __dotfile_source_path Themes $theme >/dev/null 2>&1 || \
     __dotfile_source_path Themes default >/dev/null 2>&1 || echo "Could not load a theme"
 fi
+
+# Source config
+__dotfile_source_path Config
+
+# ITERM integration
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+export TERM=xterm-256color
